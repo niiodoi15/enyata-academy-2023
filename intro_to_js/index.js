378055279d3a3b2dfcd31199ef3b61d57288d6cf
Nii Odoi <niiodoi2@gmail.com>
2023-05-26 02:14:16 +0100

    assignment three

---
+++ intro_to_js/index.js
@@ -0,0 +1,21 @@
+let myName = "Nii";
+
+typeof myName;
+
+myName = "Odoi";
+
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2022,
+    color: "Silver",
+    mileage: 5000,
+    fuelType: "Petrol",
+    engineSize: 2.5,
+    isAutomatic: true,
+    owners: ["John", "Jane", "Mike"],
+    features: ["Bluetooth", "Backup Camera", "Keyless Entry"]
+  };
+  
+
+
---
+++ intro_to_js/numbers.js
@@ -0,0 +1,9 @@
+const favoriteNumber = 7;
+const halfOfFavNum = favoriteNumber / 2;
+let isaacsFavorite = 13;
+
+let difference = isaacsFavorite - favoriteNumber;
+
+isaacsFavorite = isaacsFavorite * 26;
+
+isaacsFavorite = isaacsFavorite % 3;
